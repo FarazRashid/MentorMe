d872767ed1f5be3719ff24c49ae2a70e2eee6044
FarazRashid <muhammadfarazrashid@gmail.com>
2024-02-04 18:36:10 +0500

    Adds Reset Password Page

--- app/src/main/java/com/muhammadfarazrashid/i2106595/MainActivity.kt
+++ app/src/main/java/com/muhammadfarazrashid/i2106595/MainActivity.kt
@@ -84,7 +84,7 @@
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
-            setContentView(R.layout.forgotpasswordpage)
+            setContentView(R.layout.resetpassword)
 
 //            // Initialize the Spinner using findViewById
 //            userCountrySpinner = findViewById(R.id.userCountryTextBos)
