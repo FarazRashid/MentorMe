471209de6ba0aeb3efc2d9101466a447c257706b
FarazRashid <muhammadfarazrashid@gmail.com>
2024-02-07 18:52:51 +0500

    Adds Video Call Page

---
+++ app/src/main/java/com/muhammadfarazrashid/i2106595/VideoCallActivity.kt
@@ -0,0 +1,11 @@
+package com.muhammadfarazrashid.i2106595
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VideoCallActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_call)
+    }
+}
